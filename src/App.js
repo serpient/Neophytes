867511caf110fb3e60414d7de8af3d4697f5f0c6
serpient <littlegangwolf@gmail.com>
2018-01-11 00:50:17 -0800

    attempt 1 at home page grid layout. not very effective, will need to re-work

--- src/App.js
+++ src/App.js
@@ -6,8 +6,44 @@ import dataStructure from './DataStructure.js';
 class App extends Component {
   render() {
     return (
-      <body>
-        </body>
+      <div className="container">
+        <header className="header">
+          header
+        </header>
+        <div className="landing-image">
+          <img src="./src/assets/1.jpg" alt="landing"/>
+        </div>
+        <div className="landing-process oneOf3">
+          process1
+          </div>
+        <div className="landing-process twoOf3">
+          process2
+          </div>
+        <div className="landing-process threeOf3">
+          process3
+          </div>
+        <div className="landing-projects oneOf3">
+          project 1
+          </div>
+        <div className="landing-projects twoOf3">
+          project 2
+          </div>
+        <div className="landing-projects threeOf3">
+          project 3
+          </div>
+        <div className="landing-projects oneOf3">
+          project 4
+          </div>
+        <div className="landing-projects twoOf3">
+          project 5
+          </div>
+        <div className="landing-projects threeOf3">
+          project 6
+          </div>
+        <footer className="footer">
+          footer
+        </footer>
+      </div>
     )
   }
 }
