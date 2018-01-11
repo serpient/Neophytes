04159bbdfb205b52190b4352bc95a910bb0dd9f1
serpient <littlegangwolf@gmail.com>
2018-01-10 22:26:43 -0800

    Initial Repo with create-react-app; added initial dataStructure (user info and pages)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+import dataStructure from './DataStructure.js';
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+        </body>
+    )
+  }
+}
+
+export default App;
---
+++ src/DataStructure.js
@@ -0,0 +1,111 @@
+var user1 = {
+    username: "lilgangwolf",
+    password: "password",
+    profileImage: "https://i.imgur.com/HyGQ3GI.jpg",
+    role: ["programmer", "designer"],
+    description: "Experienced with React and design components. Looking forward to create beautiful projects with teams!",
+    techStack: ["React", "JavaScript", "AdobeXD", "HTML/CSS"],
+    bookmarks: ["000004"],
+    projects: [
+        {
+            id: "000001",
+            name: "TurtlesTab",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: null,
+            status: "completed"
+        },
+        {
+            id: "000002",
+            name: "Momentum Dash Clone",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: ["designer"],
+            status: "active"
+        },
+        {
+            id: "000003",
+            name: "Neophytes",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: ["designer", "programmer"],
+            status: "active"
+        }
+    ]
+}
+
+var user2 = {
+    username: "serpient",
+    password: "password",
+    profileImage: "https://i.imgur.com/HyGQ3GI.jpg",
+    role: ["programmer", "designer"],
+    description: "Experienced with React and design components. Looking forward to create beautiful projects with teams!",
+    techStack: ["React", "JavaScript", "AdobeXD", "HTML/CSS"],
+    bookmarks: ["000001"],
+    projects: [
+        {
+            id: "000004",
+            name: "CDN",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: null,
+            status: "completed"
+        },
+        {
+            id: "000005",
+            name: "Chingu",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: ["designer"],
+            status: "active"
+        },
+        {
+            id: "000006",
+            name: "Quizzilla",
+            description: "Clone of the momentum chrome extension, with these following design changes: (1) adding a link to github repos, (2) ability to search and pin new weather locations. We are looking for a designer to re-work the layout based off our uploaded precedents.",
+            role: ["programmer", "designer"],
+            image: ["https://i.imgur.com/INu8YMx.png", "https://i.imgur.com/cTSXV15.png", "https://i.imgur.com/B9qhUlw.png"],
+            links: {
+                github: "https://www.github.org",
+                live: "https://www.github.org",
+                mockup: "https://www.google.com"
+            },
+            openPosition: ["designer", "programmer"],
+            status: "active"
+        }
+    ]
+}
+export var dataStructure = {
+    user1,
+    user2
+};
